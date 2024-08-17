2578dd8d8b0294e57433acbce46738ed046d0133
Luciano Ramello <luchosr@gmail.com>
2024-08-17 15:05:57 +0200

    updates cargo boilerplate

---
+++ arays_6_2/target/debug/arays_6_2.d
@@ -0,0 +1,1 @@
+C:\Users\Usuario\Documents\myCode\rust\rust-by-practice\arays_6_2\target\debug\arays_6_2.exe: C:\Users\Usuario\Documents\myCode\rust\rust-by-practice\arays_6_2\src\main.rs
---
+++ arays_6_2/target/debug/deps/arays_6_2.d
@@ -0,0 +1,5 @@
+C:\Users\Usuario\Documents\myCode\rust\rust-by-practice\arays_6_2\target\debug\deps\arays_6_2.exe: src/main.rs
+
+C:\Users\Usuario\Documents\myCode\rust\rust-by-practice\arays_6_2\target\debug\deps\arays_6_2.d: src/main.rs
+
+src/main.rs:
