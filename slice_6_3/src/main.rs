513a15f05b9c41dcc9f139d18ecf0ce93e10b667
Luciano Ramello <luchosr@gmail.com>
2024-08-17 17:15:10 +0200

    adds new exercises

--- slice_6_3/src/main.rs
+++ slice_6_3/src/main.rs
@@ -1,3 +1,47 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+// Fix the errors, DON'T add new lines!
+fn exercise_1() {
+    let arr = [1, 2, 3];
+    let s1: &[i32] = &arr[0..2];
+
+     let s2: &str = "hello, world";
+
+    println!("Success!");
+}
+
+fn exercise_2() {
+    let arr: [char; 3] = ['中', '国', '人'];
+
+    let slice: &[char] = &arr[..2];
+    
+    // Modify '8' to make it work
+    // TIPS: slice( reference ) IS NOT an array, if it is an array, then `assert!` will be passed: Each of the two chars '中' and '国'  occupies 4 bytes, 2 * 4 = 8
+    assert!(std::mem::size_of_val(&slice) == 16);
+
+    println!("Success!");
+}
+
+fn exercise_3() {
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    // Fill the blanks to make the code work
+    let slice: &[i32] = &arr[1..4];
+    assert_eq!(slice, &[2, 3, 4]);
+
+    println!("Success!");
+}
+
+fn exercise_4() {
+    let s: String = String::from("hello");
+
+    let slice1: &str = &s[0..2];
+    // Fill the blank to make the code work, DON'T USE 0..2 again
+    let slice2: &str = &s[..2];
+
+    assert_eq!(slice1, slice2);
+
+    println!("Success!");
+}
